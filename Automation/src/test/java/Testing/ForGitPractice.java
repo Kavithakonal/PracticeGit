cd585776557792391ab051185ec56b3d9ef7d064
Kavitha <kavitha.konal@gmail.com>
2020-10-30 09:45:12 +0530

    adding get name method

--- Automation/src/test/java/Testing/ForGitPractice.java
+++ Automation/src/test/java/Testing/ForGitPractice.java
@@ -2,8 +2,13 @@ package Testing;
 
 public class ForGitPractice {
 
+	static String name;
+	public static String getName() {
+		return name;
+	}
 	public static void main(String[] args) {
 		System.out.println("this for gt practice");
+		System.out.println(getName() );
 	}
 
 }
